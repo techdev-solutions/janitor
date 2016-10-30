dc7cce1f02d1f27eb80ec3fd47b2587a39726e7a
Alexander Hanschke <alexander.hanschke@gmail.com>
2016-10-30 15:35:42 +0100

    Add age to items

--- pocket-api/src/main/kotlin/de/techdev/pocket/api/Item.kt
+++ pocket-api/src/main/kotlin/de/techdev/pocket/api/Item.kt
@@ -1,9 +1,26 @@
 package de.techdev.pocket.api
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.Duration
+import java.time.Instant
 
 data class Item(
         @JsonProperty("item_id") val id: Long,
         @JsonProperty("time_added") val added: Long,
         @JsonProperty("given_url") val url: String,
-        @JsonProperty("resolved_title") val title: String?)
+        @JsonProperty("resolved_title") val title: String?) {
+
+    fun label(): String{
+        if (title.isNullOrEmpty()) {
+            return url
+        }
+
+        return title!!
+    }
+
+    fun age(): Long {
+        val instant = Instant.ofEpochSecond(added)
+        return Duration.between(instant, Instant.now()).toDays()
+    }
+
+}
